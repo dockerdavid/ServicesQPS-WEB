121e1e029a6f61fd61a6157fe6c3c781d8934453
Andrés Eduardo Sánchez Estévez <andrespersonaluser@gmail.com>
2025-03-09 20:02:56 -0500

    feat:

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { fileURLToPath, URL } from 'node:url';
+
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
@@ -11,19 +11,4 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    proxy: {
-
-      '/api': {
-        target: 'https://api.servicesqps.com',  
-        changeOrigin: true,  
-        rewrite: (path) => path.replace(/^\/api/, ''),  
-      },
-    },
-  },
 });
