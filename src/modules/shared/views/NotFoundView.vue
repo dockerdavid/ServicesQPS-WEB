fc5ed84e98eeb8f5ccec3d4d05b5fd242b8c6d74
Andrés Eduardo Sánchez Estévez <andrespersonaluser@gmail.com>
2025-03-13 16:32:42 -0500

    feat: Permitir regresar desde notfoundview

--- src/modules/shared/views/NotFoundView.vue
+++ src/modules/shared/views/NotFoundView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="p-4 h-[100vh]">
-        <router-link class="text-2xl" :to="{ name: 'dashboard' }">Go back</router-link>
+        <router-link class="text-2xl" :to="roleRoutes[userRole][0]">Go back</router-link>
         <div class="h-[90%] flex place-content-center">
             <img class="w-100" src="../../../assets/not-found.svg" alt="">
         </div>
@@ -9,6 +9,12 @@
 </template>
 
 <script setup lang="ts">
+
+import roleRoutes from '../../../../src/router/role-routes';
+import { useUserStore } from '../../../../src/store/user.store';
+
+const userRole = useUserStore().userData?.role.name.toLowerCase() as keyof typeof roleRoutes;
+
 
 </script>
 
