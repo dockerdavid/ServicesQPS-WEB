678a5e0477b0bb744825bd9482bc349ee66ba59b
Andrés Eduardo Sánchez Estévez <andrespersonaluser@gmail.com>
2025-03-14 00:30:29 -0500

    feat: obtener costos semanales

--- src/modules/costs/CostsDataView.vue
+++ src/modules/costs/CostsDataView.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
+import { apiServicesQps } from "../../../src/api/api";
+import LoadingButton from "../shared/components/LoadingButton.vue";
 import GenericDataView from "../shared/views/GenericDataView.vue";
 import { CostsServices } from "./costs.services";
+import { useGlobalStateStore } from "../../../src/store/auth.store";
+import { computed, ref } from "vue";
+import moment from "moment";
 
 
+const reportDate = ref(new Date());
+
+const formattedDate = computed(() => 
+  reportDate.value ? moment(reportDate.value).format('YYYY-MM-DD') : ''
+);
+
+const { setIsLoading } = useGlobalStateStore();
 
 const fetchCosts = async (page: number, rows: number) => {
     return await CostsServices.getCosts(page, rows);
@@ -17,6 +29,30 @@ const searchCost = async (searchWord: any, page: number, rows: number) => {
     return await CostsServices.searchCost(searchWord, page, rows)
 };
 
+const getWeeklyCosts = async () => {
+    setIsLoading(true);
+    try {
+        const { data } = await apiServicesQps.get(`/reports/costos-semana/${formattedDate.value}`, {
+            responseType: 'blob'
+        });
+
+
+        const url = window.URL.createObjectURL(new Blob([data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', `reporte-semana-${formattedDate.value}.pdf`);
+        document.body.appendChild(link);
+        link.click();
+
+        document.body.removeChild(link);
+        window.URL.revokeObjectURL(url);
+    } catch (error) {
+        console.error("Error al obtener el reporte:", error);
+    } finally {
+        setIsLoading(false);
+    }
+};
+
 </script>
 
 <template>
@@ -24,6 +60,11 @@ const searchCost = async (searchWord: any, page: number, rows: number) => {
         { field: 'date', name: 'Date' },
         { field: 'description', name: 'Description' },
         { field: 'amount', name: 'Amount' },
-    ]"  :fetch-data="fetchCosts" :delete-data="deleteCost"
-         :search-data="searchCost" />
+    ]" :fetch-data="fetchCosts" :delete-data="deleteCost" :search-data="searchCost">
+
+        <template #header-button>
+            <LoadingButton @click="getWeeklyCosts" label="Export costs"/>
+        </template>
+
+    </GenericDataView>
 </template>
